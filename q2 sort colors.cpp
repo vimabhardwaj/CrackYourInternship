dfa61e535aae049d6a874af83be6bafecc2a72ca
vimabhardwaj <82300531+vimabhardwaj@users.noreply.github.com>
2022-05-05 14:33:26 +0530

    Update q2 sort colors.cpp

--- q2 sort colors.cpp
+++ q2 sort colors.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
+        int n=nums.size();
+int s=0;
+int m=0;      // DNF SORT ALGORITHM
+int e=n-1;
+
+    while(m<=e){
+        if(nums[m]==0){ 
+            swap(nums[s++],nums[m++]);
+            
+        }
+        else if(nums[m]==2){
+            swap(nums[m],nums[e--]);
+        }
+        else{
+            m++;
+        }
     }
+}
+
+    
 };
