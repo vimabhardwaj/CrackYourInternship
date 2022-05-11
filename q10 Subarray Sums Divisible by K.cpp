8ee51db10c4c4bf53187829b52cb29919252569d
Vima Bhardwaj <82300531+vimabhardwaj@users.noreply.github.com>
2022-05-11 20:17:20 +0530

    Update q10 Subarray Sums Divisible by K.cpp

--- q10 Subarray Sums Divisible by K.cpp
+++ q10 Subarray Sums Divisible by K.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-        int res=0;
-        for(int i=0; i<nums.size();i++){
-             int sum=0;
-            for(int j=i; j<nums.size();j++){
-              
-                sum= sum+nums[j];
-                if(sum%k==0)
-                    res++;
+        unordered_map<int,int> mpp;
+        int count=0;
+        int cumsum=0;
+        mpp[0]++;
+        for(int i=0;i<nums.size();i++){
+            cumsum+=nums[i];
+            int rem = cumsum%k;
+            if(rem<0){
+                rem+=k;
             }
+            if(mpp.count(rem)>0){
+                count+=mpp[rem];
+            }
+            mpp[rem]++;
         }
-        return res;
+        return count;
     }
 };
