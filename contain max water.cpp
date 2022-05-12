58f9cea7c8471f5aed6e6b94ba6c3af1db7d3554
Vima Bhardwaj <82300531+vimabhardwaj@users.noreply.github.com>
2022-05-12 21:24:43 +0530

    Create contain max water.cpp

---
+++ contain max water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int i=0;
+        int j= height.size()-1;
+        int water= 0;
+        
+        while(i<j){
+            int w= j-i;
+            int h= min(height[i],height[j]);
+            
+            water= max(water,(w*h));
+            if(height[i]<height[j])
+                i++;
+            else
+                j--;
+        }
+        
+        
+        return water;
+    }
+};
